044feba61cd4a822c3dd4bc3c59286caca469c5f
valek228123 <valentinveremchuk.2007@gmail.com>
2025-11-23 15:03:40 +0300

    Initial commit for Render deployment

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-import os
-BOT_TOKEN = os.getenv("BOT_TOKEN") 
-DATABASE_URL = os.getenv("DATABASE_URL")
+BOT_TOKEN = "8410145959:AAF2RL0gKcOSaad5CtB1zgfeqYSrl75Bphg"
+DATABASE_URL = "postgresql://neondb_owner:npg_y49gsEADRXth@ep-silent-wind-ah8rnmi0-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
